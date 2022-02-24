008152567045f042a51aee00a14705210c3154c7
nikitos03 <83069842+nikitos03@users.noreply.github.com>
2022-02-24 13:18:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ЕНГ_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shuffle();
+        }
+
+        static void Shuffle()
+        {
+            int[] massiv = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var str = string.Join(" ", massiv);
+
+            Console.WriteLine(str);
+
+            Random random = new Random();
+            for (int i = massiv.Length - 1; i >= 1; i--)
+            {
+                int randomIndex = random.Next(i + 1);
+                int shuffleIndex = massiv[randomIndex];
+                massiv[randomIndex] = massiv[i];
+                massiv[i] = shuffleIndex;
+                Console.WriteLine(massiv[i]);
+            }
+        }
+    }
+}
